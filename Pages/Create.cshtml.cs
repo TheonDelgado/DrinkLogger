51c618ce48ba16507b31e890f54714e953aa8264
ApolloMix <delgado_theon@yahoo.com>
2022-04-26 02:08:50 -0400

    Initial Create, set up database, getting error

---
+++ Pages/Create.cshtml.cs
@@ -0,0 +1,46 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using DrinkLogger.Pages.Models;
+using Microsoft.Data.Sqlite;
+
+namespace DrinkLogger.Pages
+{
+    public class Create : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public Create(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DrinkLog DrinkLog { get; set; } 
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            using(var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = $@"INSERT INTO DrinkLog(Date, Quantity)
+                                        VALUES(
+                                                '{DrinkLog.Date}',
+                                                '{DrinkLog.Quantity}')";
+                command.ExecuteNonQuery();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Models/DrinkLog.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkLogger.Pages.Models
+{
+    public class DrinkLog
+    {
+        public  int Id { get; set; }
+        public  DateTime Date { get; set; }
+        public  int Quantity { get; set; }
+    }
+}
